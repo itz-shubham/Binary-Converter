dbd88989ef3d0cceb6f0f206887d9a990f0229b8
surajgirioffl <surajkumargiri2002@gmail.com>
2022-08-26 21:14:21 +0530

    Refactor: format the code in better manner
    Format the code using default formatter of vscode.
    
    Signed-off-by: @surajgirioffl

--- main.c
+++ main.c
@@ -1,44 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void tobinary(int n, char prefix[3]){
-    int i ;
+void tobinary(int n, char prefix[3])
+{
+    int i;
     int a[4];
 
     printf("%s", prefix);
 
-    for (i = 4; i >= 0; i--){
+    for (i = 4; i >= 0; i--)
+    {
         a[i] = n % 2;
-        n = n/2;
+        n = n / 2;
     }
 
-    for (i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++)
+    {
         printf("%d", a[i]);
     }
     printf(" ");
-    
 }
 
-void converter(char c){
+void converter(char c)
+{
     int i;
-    char list1[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    char list2[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char list1[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char list2[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    for ( i = 0; i < 26; i++){
-        if (c == list1[i]){
-            tobinary(i+1, "010");
+    for (i = 0; i < 26; i++)
+    {
+        if (c == list1[i])
+        {
+            tobinary(i + 1, "010");
             i = 26;
         }
-        if (c == list2[i]){
-            tobinary(i+1, "011");
+        if (c == list2[i])
+        {
+            tobinary(i + 1, "011");
             i = 26;
         }
     }
 }
 
+void main()
+{
 
-
-void main(){
-    
     int i;
     char text[10];
 
@@ -49,6 +54,6 @@ void main(){
     {
         converter(text[i]);
     }
-    
+
     printf("\n");
 }
